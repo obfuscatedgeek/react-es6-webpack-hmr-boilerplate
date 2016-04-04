e7e2508f58773539e4c2be85ce682190a09b7d0b
Ejaz Bawasa <bawasa.ejaz@gmail.com>
2016-04-04 03:53:26 +0200

    Base configs set

---
+++ installl.sh
@@ -0,0 +1,1 @@
+npm i -D webpack webpack-dev-server react-hot-loader && npm i -D babel-core babel-loader && npm i -D babel-preset-es2015 babel-preset-react && npm i --save react react-dom history react-router
